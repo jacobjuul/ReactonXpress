fc6d0f65ffc39c2f8f2382e55dedd0bfc75751fa
Jacob Juul Christiansen <jacob.juul@gmail.com>
2015-12-08 11:13:52 +0100

    more functions

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -1,11 +1,35 @@
 var React = require('react');
+var action = require('./../actions/GroceryItemActionCreator.jsx')
 
 var GroceryItem = React.createClass({
+    delete: function(e) {
+        e.preventDefault();
+        action.delete(this.props.item);
+    },
+    togglePurchased: function(e) {
+        e.preventDefault();
+        if (this.props.item.purchased) {
+            action.unbuy(this.props.item);
+        }  else {
+            action.buy(this.props.item);
+        }
+    },
+
     render: function() {
         return (
+            
              <li className={this.props.item.purchased ? 'strikethrough' : ''}>
               {this.props.item.name}
+              <form className="three columns" onSubmit={this.togglePurchased}> 
+                <button className={this.props.item.purchased ? "" : "button-primary"}>
+                    {this.props.item.purchased ? "Unbuy" : "Buy"}
+                </button>
+              </form>
+              <form className="three columns" onSubmit={this.delete}> <button>x</button></form>
             </li>
+
+          
+
         )
     }
 });
--- app/stores/GroceryItemStore.jsx
+++ app/stores/GroceryItemStore.jsx
@@ -23,6 +23,28 @@ function GroceryItemStore() {
     triggerListeners();
   }
 
+  function setGroceryItemBought(item, isBought) {
+    var _item = items.filter(function(a){
+      return a.name === item.name;
+    })[0];
+
+    item.purchased = isBought || false;
+     triggerListeners();
+  }
+
+
+  function deleteGroceryItem(item) {
+    var index;
+    items.filter(function(_item, _index) {
+      if (_item.name == item.name) {
+        index = _index;
+      } 
+    });
+
+    items.splice(index, 1);
+    triggerListeners();
+  }
+
   function onChange(listener) {
     listeners.push(listener);
   }
@@ -33,12 +55,23 @@ function GroceryItemStore() {
     });
   }
 
+  
+
   dispatcher.register(function(event) {
     var split = event.type.split(':');
     if (split[0] === 'grocery-item') {
       switch(split[1]) {
         case 'add':
           addGroceryItem(event.payload);
+          break;
+        case 'delete':
+          deleteGroceryItem(event.payload);
+          break;
+        case 'buy':
+          setGroceryItemBought(event.payload, true);
+          break;
+        case 'unbuy':
+          setGroceryItemBought(event.payload, false);
           break;
       }
     }
